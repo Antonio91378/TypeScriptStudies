cbc3aac6661ec7ff69b54bf0a9b239ad84c7beb9
Antonio Dias <antoniovictor200@gmail.com>
2022-08-09 15:00:48 -0300

    inicio

---
+++ typescript-curso-1-arquivos-iniciais/app/models/Negociacao.ts
@@ -0,0 +1,24 @@
+export class Negociacao {
+  _data;
+  _quantidade;
+  _valor;
+
+  constructor(data, quantidade, valor) {
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
+  }
+
+  get Data() {
+    return this._data;
+  }
+  get Quantidade() {
+    return this._quantidade;
+  }
+  get Valor() {
+    return this._valor;
+  }
+  get Volume() {
+    return this._quantidade * this._valor;
+  }
+}
